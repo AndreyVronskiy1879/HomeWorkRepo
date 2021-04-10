27986f0c84fcaf16f01b517725ccadb9d607486a
Andrey Vronskiy <rainbow191991@gmail.com>
2021-04-10 15:21:23 +0300

    1st file

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ArraysHW2
+{
+    class Program
+    {
+   
+        static void Main(string[] args)
+        {
+          
+           
+
+           
+        }
+    }
+}
