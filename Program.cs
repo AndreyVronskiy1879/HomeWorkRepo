e3dd5e4556b405b3aa29d34fdadaeb21eb582d3a
Andrey Vronskiy <rainbow191991@gmail.com>
2021-04-10 16:44:41 +0300

    1st HW string

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StringHW1
+{
+    class Program
+    {
+//        Write a program in C# Sharp to find the length of a string without using library function.
+//Test Data :
+//Input the string : w3resource.com
+//Expected Output :
+
+//Length of the string is : 15 
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
