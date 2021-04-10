ab33c7a2bae962f738673bf8298f954e371ae0cd
Andrey Vronskiy <rainbow191991@gmail.com>
2021-04-10 17:55:05 +0300

    1st file HW3 string

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StringHW3
+{
+    class Program
+    {
+//        Write a program in C# Sharp to print individual characters of the string in reverse order. Go to the editor
+//Test Data :
+//Input the string : w3resource.com
+//Expected Output :
+
+//The characters of the string in reverse are : 
+
+//m o  c.e c  r u  o s  e r  3  w
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
