8bfbf6e2713bf7d29c9991c2da6195b929ec6e9a
Andrey Vronskiy <rainbow191991@gmail.com>
2021-04-10 17:45:00 +0300

    1st file HW2 string

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StringHW2
+{
+    class Program
+    {
+//        Write a program in C# Sharp to separate the individual characters from a string. Go to the editor
+//Test Data :
+//Input the string : w3resource.com
+//Expected Output :
+
+//The characters of the string are : 
+//w  3  r e  s o  u r  c e.c  o m
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
