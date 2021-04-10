a3349d302eea8e268778f26fb114b7181a51737f
Andrey Vronskiy <rainbow191991@gmail.com>
2021-04-10 20:05:43 +0300

    HW5 string

--- Program.cs
+++ Program.cs
@@ -13,7 +13,24 @@ namespace StringHW5
         //The total number of consonant in the string is : 12 
         static void Main(string[] args)
         {
-           
+            string str = "Welcome to w3resource.com";
+            var glas = 0;
+            var sogl = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+
+                if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+                {
+                    glas++;
+                }
+                else if ((str[i] >= 'a' && str[i] <= 'z'))
+                {
+                    sogl++;
+                }
+            }
+            Console.Write($"The total number of vowel in the string is : {glas} ");
+            Console.Write($" The total number of consonant in the string is : {sogl}");
 
         }
   
