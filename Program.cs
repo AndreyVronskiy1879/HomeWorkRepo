367510da8e348626807976ee125dc99072917bb0
Andrey Vronskiy <rainbow191991@gmail.com>
2021-04-10 15:32:40 +0300

    3th HW arrays

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace ArraysHW3
     {
         static void Main(string[] args)
         {
-            
+            int[] nums = new int[3] { 2, 5, 8 };
+            nums[0] = 2;
+            nums[1] = 5;
+            nums[2] = 8;
+            int summ = nums[0] + nums[1] + nums[2];
+            Console.WriteLine(summ);
         }
     }
 }
