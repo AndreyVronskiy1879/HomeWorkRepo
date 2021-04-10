ee63626b16a8f16d1d39507a52eceb7274ab9528
Andrey Vronskiy <rainbow191991@gmail.com>
2021-04-10 19:06:24 +0300

    1st file HW4 string

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StringHW4
+{
+    class Program
+    {
+//        8. Write a program in C# Sharp to copy one string to another string. Go to the editor
+//Test Data :
+//Input the string : This is a string to be copied.
+//Expected Output :
+//The First string is : This is a string to be copied. 
+//The Second string is : This is a string to be copied. 
+//Number of characters copied : 31 
+        static void Main(string[] args)
+        {
+            
+            
+        }
+    }
+}
