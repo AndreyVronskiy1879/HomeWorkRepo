89e1bb8fa9384079e1dbe031f59388e9a16f2116
Andrey Vronskiy <rainbow191991@gmail.com>
2021-04-10 20:02:58 +0300

    1st file HW5 string

--- Program.cs
+++ Program.cs
@@ -4,16 +4,18 @@ namespace StringHW5
 {
     class Program
     {
-//        Write a program in C# Sharp to count a total number of vowel or consonant in a string. Go to the editor
-//Test Data :
-//Input the string : Welcome to w3resource.com
-//Expected Output :
+        //        Write a program in C# Sharp to count a total number of vowel or consonant in a string. Go to the editor
+        //Test Data :
+        //Input the string : Welcome to w3resource.com
+        //Expected Output :
 
-//The total number of vowel in the string is : 9 
-//The total number of consonant in the string is : 12 
+        //The total number of vowel in the string is : 9 
+        //The total number of consonant in the string is : 12 
         static void Main(string[] args)
         {
-            
+           
+
         }
+  
     }
 }
